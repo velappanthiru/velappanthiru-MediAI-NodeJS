e60f96777449821ef6309e403d07d234f20a8e7f
velappan <velappan.t@appstarsapplications.in>
2025-06-10 11:43:41 +0530

    add network in port

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use((err, req, res, next) => {
 
 // Sync database & start server
 sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 });
